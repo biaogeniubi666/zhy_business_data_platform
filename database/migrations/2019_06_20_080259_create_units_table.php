dab3fffe89601b6a4de5de66f1a43105895270f4
biaoge <365056996@qq.com>
2019-07-19 13:16:35 +0800

    0719最新

---
+++ database/migrations/2019_06_20_080259_create_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    // 生成数据列！
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('unit_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
